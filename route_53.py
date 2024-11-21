16e4f14ea60bfcbaee5c4014d71cf01b85998f6b
LEndres-Inmar <luke.endres@inmar.com>
2024-11-21 13:40:16 -0600

    A + CNAMES being retrieved

--- route_53.py
+++ route_53.py
@@ -1,5 +1,10 @@
-import boto3
+#!/usr/bin/env python
 
+import boto3
+from botocore.exceptions import ClientError
+import os
+import json
+import pprint
 
 
 access_key_id = os.environ['AWS_ACCESS_KEY_ID']
@@ -8,33 +13,36 @@ session_token = os.environ['AWS_SESSION_TOKEN']
 assumeRoleName = 'OrganizationAccountAccessRole'
 
 us_regions = ["us-east-1","us-east-2","us-west-1","us-west-2"]
-print('[*] Initiating boto3 session...')
 session = boto3.session.Session(aws_access_key_id=access_key_id, aws_secret_access_key=secret_access_key, aws_session_token=session_token, region_name='us-east-1')
 
+route53_client = session.client('route53')
 
 
 
-aws_profile = '...'
-zone_id = 'Z2A...'
-max_records = 1000
-
-
-route53 = session.client('route53')
 
 
 
-dns_records = []
+def get_routes(accounts):
+    for account in accounts:
+        # List the record sets in the specified hosted zone
+        response = route53_client.list_resource_record_sets(HostedZoneId="Z2J5S9MTWWRUXF")
 
-dns_in_iteration = route53.list_resource_record_sets(HostedZoneId=zone_id)
-dns_records.extend(dns_in_iteration['ResourceRecordSets'])
+        a_records = []
+        cname_records = []
 
-while len(dns_records) < max_records and 'NextRecordName' in dns_in_iteration.keys():
-    next_record_name = dns_in_iteration['NextRecordName']
-    print('listing next set: ' + next_record_name)
-    dns_in_iteration = route53.list_resource_record_sets(HostedZoneId=zone_id, StartRecordName=next_record_name)
-    dns_records.extend(dns_in_iteration['ResourceRecordSets'])
+        # Loop through the record sets and filter A and CNAME records
+        for record in response['ResourceRecordSets']:
+            if record['Type'] == 'A':
+                a_records.append(record)
+            elif record['Type'] == 'CNAME':
+                cname_records.append(record)
 
-print('records found: ' + str(len(dns_records)))    
-for record in dns_records:
-    if record['Type'] == 'CNAME':
-        print(record['Name'])
+        pprint.pp(a_records)
+        return a_records, cname_records
+
+
+if __name__ == '__main__':
+    # specific aws accounts
+    grocery_ecommerce_accounts = ['inmar-eretail-deploy'] #,'inmar-eretail-log','inmargrocerydev','inmargroceryprod']
+
+    get_routes(grocery_ecommerce_accounts)
