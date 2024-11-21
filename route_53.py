c43e4a1763efcc80b10838685c7b0cfe5fe2dd1b
LEndres-Inmar <luke.endres@inmar.com>
2024-11-21 16:33:45 -0600

    Update route_53.py
    I don't have permissions? "luke.endres@inmar.com is not authorized to access this resource"

--- route_53.py
+++ route_53.py
@@ -25,13 +25,6 @@ org_client = session.client('organizations')
 
  
 
-'''
-Alright... so without specifying an account, it runs off the access keys and secrets you give it. Neat.
-
-Inmar Consolidated -> assume roll?
-'''
-
-
 
 
 
@@ -48,51 +41,11 @@ def get_accounts():
 
     for page in accounts_page_iterator:  
         for account in page['Accounts']:
-            # account is a dictionary
             accounts_list.append(account)
 
-            #print('   '+str(account['Name']))
-            #print(account)
-    #print('[*] Accounts retrieved!')
-    
     return accounts_list
 
 
-'''
-def get_all_hosted_zone_ids():
-    # Create a Route 53 client
-    #client = boto3.client('route53')
-
-    # Initialize a list to store hosted zone IDs
-    hosted_zone_ids = []
-
-    # Start with the first request
-    next_marker = None
-    while True:
-        # Call list_hosted_zones with the next_marker if it exists
-        if next_marker:
-            response = route53_client.list_hosted_zones(Marker=next_marker)
-        else:
-            response = route53_client.list_hosted_zones()
-
-        # Extract the hosted zone IDs and add them to the list
-        hosted_zone_ids.extend([zone['Id'] for zone in response['HostedZones']])
-
-        # Check if there is a next page (next_marker), if so, continue
-        next_marker = response.get('NextMarker')
-        if not next_marker:
-            break  # No more pages, break out of the loop
-
-    #print(f'hosted zones: {hosted_zone_ids}')
-    return hosted_zone_ids
-
-# Example usage
-hosted_zone_ids = get_all_hosted_zone_ids()
-
-# Print all hosted zone IDs
-for zone_id in hosted_zone_ids:
-    print(zone_id)
-'''
 
 
 
@@ -111,27 +64,6 @@ def assume_role(account_id, role_name):
     credentials = response['Credentials']
     
     return credentials
-
-
-
-
-def get_routes(accounts):
-    for account in accounts:
-        # List the record sets in the specified hosted zone
-        response = route53_client.list_resource_record_sets(HostedZoneId="Z2J5S9MTWWRUXF")
-
-        a_records = []
-        cname_records = []
-
-        # Loop through the record sets and filter A and CNAME records
-        for record in response['ResourceRecordSets']:
-            if record['Type'] == 'A':
-                a_records.append(record)
-            elif record['Type'] == 'CNAME':
-                cname_records.append(record)
-
-        pprint.pp(a_records)
-        return a_records, cname_records
 
 
 
@@ -160,11 +92,35 @@ def get_hosted_zone_ids():
 
 
 
-def zones(account):
+
+
+
+
+def get_routes_from_zone(zone_id):
+    for zone in zones:
+        # List the record sets in the specified hosted zone
+        response = route53_client.list_resource_record_sets(HostedZoneId="Z2J5S9MTWWRUXF")
+
+        a_records = []
+        cname_records = []
+
+        # Loop through the record sets and filter A and CNAME records
+        for record in response['ResourceRecordSets']:
+            if record['Type'] == 'A':
+                a_records.append(record)
+            elif record['Type'] == 'CNAME':
+                cname_records.append(record)
+
+        pprint.pp(a_records)
+        return a_records, cname_records
+
+
+
+def get_zones_from_account_id(account_id):
     #for account in get_accounts():
 
 
-    assume_role_arn = "arn:aws:iam::" + account['Id'] + ":role/" + assumeRoleName
+    assume_role_arn = "arn:aws:iam::" + account_id + ":role/" + assumeRoleName
     assumedRoleObject = sts_client.assume_role(RoleArn=assume_role_arn, RoleSessionName="OrgAssumeRole")
     
     credentials = assumedRoleObject['Credentials']
@@ -180,12 +136,16 @@ def zones(account):
         print(f"\t{zone['Name']} -> {zone['Id']}")
     print("\n")
 
+    return response['HostedZones']
 
-def get_account_info(account_name):
+
+def get_account_details(account_name):
     '''To retrieve only a specific AWS account (based on the account name or account ID)
     using AWS Organizations, you can filter the results after calling the list_accounts
     API, as AWS Organizations does not support filtering by account name directly in the
-    API request itself.'''
+    API request itself.
+    
+    :returns:'''
 
     accounts_paginator = org_client.get_paginator('list_accounts')
     accounts_page_iterator = accounts_paginator.paginate()
@@ -207,43 +167,12 @@ if __name__ == '__main__':
 
 
     for account in accounts:
-        account_details = get_account_info(account)
-        zones(account_details)
-        #print(get_account_info(account)['Id'])
+        account_details = get_account_details(account)
+        zones = get_zones_from_account_id(account_details['Id'])
 
-
-    #print(accounts[1]['Id'])
-
-    #zones(accounts[1])
-
-    '''for account in accounts:
-        print(account['Id'])
+        # loop through each zone
+        for zone in zones:
+            get_routes_from_zone(zone['Id'])
         
-        try:
-            assume_role_arn = "arn:aws:iam::" + account['Id'] + ":role/" + assumeRoleName
-            assumedRoleObject = sts_client.assume_role(RoleArn=assume_role_arn, RoleSessionName="OrgAssumeRole")
-            credentials = assumedRoleObject['Credentials']
-
-            for region in us_regions:
-                ec2_client=boto3.client('route53',
-                    aws_access_key_id=credentials['AccessKeyId'],
-                    aws_secret_access_key=credentials['SecretAccessKey'],
-                    aws_session_token=credentials['SessionToken'],
-                    region_name=region
-                )
-
-                reservations = ec2_client.describe_instances()['Reservations']
-                instance_list = []
-                for reservation in reservations:
-                    print(type(reservation))
-
-                    pprint.pp(f'reservation: {reservation}')
-                    print('\n\n\n')
-                    for instance in reservation['Instances']:
-                        pprint.pp(f'instance: {instance}')
-                        instance_list.append(instance)
-                        # ec2_list = ec2_client.describe_instances().append(ec2_list)
-            # print(instance_list)
-            # return instance_list
-        except ClientError as error:
-            continue'''
+        #for zone_group in account_details
+        #get_zones_from_account(account_details)
